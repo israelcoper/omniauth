0c99f11a709537276cba362fd7d621e373b0e989
Israel B. Coper <coper_israelb@yahoo.com>
2011-08-31 10:54:16 +0800

    Use devise authentication.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omniauth::Application.config.secret_token = 'cfc440139436966861b0f01ee3ac40d1a8b0dbfdb5b59724c7b72845ede850a060ec35e004c4a7bc967d957c8712ff4cfc55b7cbc3c440665a0cf6215d462dbf'
